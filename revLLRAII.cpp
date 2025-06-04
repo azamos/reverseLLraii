056c33adfe6e8e57e9d1039f7489428838705915
amos zohar <azamoszohar@gmail.com>
2025-06-04 13:50:36 +0300

    added remove functionality and a test

--- revLLRAII.cpp
+++ revLLRAII.cpp
@@ -79,6 +79,37 @@ public:
         linkSize++;
     }
 
+    bool remove(int valueToDel)
+    {
+        if (isEmpty())
+            return false;
+
+        if (head->getValue() == valueToDel)
+        {
+            head = std::move(head->releaseNext());
+            linkSize--;
+            return true;
+        }
+
+        Node *curr = head.get();
+        while (curr->getNext() && curr->getNext()->getValue() != valueToDel)
+        {
+            curr = curr->getNext();
+        }
+        if (!curr->getNext()) /*Means valueToDel was not found in the list*/
+            return false;
+
+        /*Being here means: the next node is the node to be deleted.
+        Must bypass it and get to next next*/
+        std::unique_ptr<Node> nextNext = std::move(curr->getNext()->releaseNext());
+        std::unique_ptr<Node> nodeToDel = std::move(curr->releaseNext()); /*RAII - the ownership of next,
+         which is the node needed to be deleted, will be transfered to nodeToDel, and so the resource will
+         be released once leaving the scope of this function.*/
+        curr->setNext(std::move(nextNext));
+        linkSize--;
+        return true;
+    }
+
     bool isEmpty() const
     {
         return head == nullptr;
@@ -133,6 +164,30 @@ bool test1()
     return l1.size() == SIZE1 && l1.size() == Node::getConstructionCounter() && !Node::getDestructorCounter();
 }
 
+bool test2()
+{
+    std::cout << std::endl
+              << "Starting test2 : inserting 1,...,100";
+    std::cout << std::endl
+              << "list should print 100,99,....,1";
+    LinkedList l2;
+    for (int i = 1; i <= SIZE1; i++)
+    {
+        l2.insert(i);
+    }
+    l2.print();
+    std::cout << std::endl
+              << "Removing 100,...,1";
+    std::cout << std::endl
+              << "list should print: 'List is empty. Nothing to print.' ";
+    for (int i = SIZE1; i > 0; i--)
+    {
+        l2.remove(i);
+    }
+    l2.print();
+    return l2.size() == 0 && Node::getConstructionCounter() == Node::getDestructorCounter();
+}
+
 int main()
 {
     int score = SIZE1;
@@ -140,7 +195,13 @@ int main()
     {
         std::cout << std::endl
                   << "Test 1 failed";
-        score -= 100;
+        score -= 50;
+    }
+    if (!test2())
+    {
+        std::cout << std::endl
+                  << "Test 2 failed";
+        score -= 50;
     }
     if (score == SIZE1)
         std::cout << std::endl
